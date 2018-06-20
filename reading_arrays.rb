d9493b30d1ae847aba9ccc2aa93e05f9c1ded0fc
Sarah Li <sarcl@hotmail.com>
2018-06-20 15:22:18 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -25,10 +25,12 @@ end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  student_name.first
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  student_name.last
 end
 
 def first_second_and_third_students
