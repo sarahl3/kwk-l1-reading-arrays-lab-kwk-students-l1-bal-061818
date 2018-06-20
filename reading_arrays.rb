41e7b54bbf8ea6a9d05d6db227d89e6b1ed5d5d0
Sarah Li <sarcl@hotmail.com>
2018-06-20 15:35:37 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -10,7 +10,7 @@ student_names = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
-  student_names[00]
+  student_names[0]
 end
 
 def fourth_student_by_index
